f8991a61efba8f9ff95fd73f9ef185a14241ea88
Zahid Khan <imzahid40021@gmail.com>
2025-05-09 04:39:07 -0700

    Refactored code

--- test.rb
+++ test.rb
@@ -1,19 +1,23 @@
 require_relative 'setup'
 
+def truncate_to_2_decimals(num)
+  (num * 100).floor / 100.0
+end
+
 basket = Basket.new(products: PRODUCTS, delivery_rules: DELIVERY_RULES, offers: OFFERS)
 basket.add('B01')
 basket.add('G01')
-puts "Total: $#{'%.2f' % basket.total}" # $37.85
+puts "Total: $#{truncate_to_2_decimals(basket.total)}" # $37.85
 
 basket = Basket.new(products: PRODUCTS, delivery_rules: DELIVERY_RULES, offers: OFFERS)
 basket.add('R01')
 basket.add('R01')
-puts "Total: $#{'%.2f' % basket.total}" # $54.37
+puts "Total: $#{truncate_to_2_decimals(basket.total)}" # $54.37
 
 basket = Basket.new(products: PRODUCTS, delivery_rules: DELIVERY_RULES, offers: OFFERS)
 basket.add('R01')
 basket.add('G01')
-puts "Total: $#{'%.2f' % basket.total}" # $60.85
+puts "Total: $#{truncate_to_2_decimals(basket.total)}" # $60.85
 
 basket = Basket.new(products: PRODUCTS, delivery_rules: DELIVERY_RULES, offers: OFFERS)
 basket.add('B01')
@@ -21,4 +25,4 @@ basket.add('B01')
 basket.add('R01')
 basket.add('R01')
 basket.add('R01')
-puts "Total: $#{'%.2f' % basket.total}" # $98.27
+puts "Total: $#{truncate_to_2_decimals(basket.total)}" # $98.27
